df19e7e50c7710254f8026053c9176ec99a81975
extropye <extropye@gmail.com>
2014-03-05 22:59:38 +0400

    Initial import

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by feature on 05.03.2014.
+//  Copyright (c) 2014 extropye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
